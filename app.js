278a6f4d30f3ceae2a5fc4754d3da09a3c31593e
Aniket Sharma <89375574+aniketsham@users.noreply.github.com>
2024-08-14 23:41:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ config({path:"./config/config.env"})
 
 app.use(cors({
     origin:process.env.FRONTEND_URL,
-    methods:["GET","POST","PUT","DELETE"],
+    methods:["GET","POST","PUT","DELETE","OPTIONS"],
     credentials:true,
     
 }))
