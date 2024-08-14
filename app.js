c1adfe2cded89557255c8c9b7cee4b1db1c58693
Aniket Sharma <89375574+aniketsham@users.noreply.github.com>
2024-08-14 21:55:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ app.use(cors({
     origin:process.env.FRONTEND_URL,
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
+    
 }))
-
+app.options('*', cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
