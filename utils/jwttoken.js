6f8022eaa7f36e3355372f6a0ee142104b95e6ac
Aniket Sharma <89375574+aniketsham@users.noreply.github.com>
2024-08-15 00:59:52 +0530

    Update jwttoken.js

--- utils/jwttoken.js
+++ utils/jwttoken.js
@@ -6,6 +6,7 @@ export const sendToken=(user,statusCode,res,message)=>{
             Date.now()+process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly:true
+        secure: true
     };
     res.status(statusCode).cookie("token",token,options).json({
         success:true,
